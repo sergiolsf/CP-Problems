35eb96927f9cc178fd410ba5e1d5718d9a679fb1
sergiolsf <sergiopoligota@gmail.com>
2024-11-17 14:21:58 -0300

    Template for Problems

---
+++ Template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define all(x) (x).begin(), (x).end()
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+void fastio() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+signed main() {
+    fastio();
+
+
+    
+    return 0;
+}
